03cfb78df6f66e3c36b8aa12fdb10dbe9244a2d1
bigiraneza-car <b.igiraneza@alustudent.com>
2026-06-03 20:23:52 +0200

    added comment to exlain the use of each line

--- secure_data.sh
+++ secure_data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+#create the secure_data function to verify if the directory active_logs is found
 secure_data(){
+	#uses the if condition to verify if the directory exists.
 	if {! -d "active_logs" }; then
 		echo "ERROR: active-logs not found"
 		return 1
 	fi
+	# now have to change the permission so that the owner can read, write, and execute the directory.
 	chmod 700 active-logs
 	echo "active_logs is now secured"
 	ls -ld active_logs
